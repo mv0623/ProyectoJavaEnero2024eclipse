f0d20ea0f4d327b70afd2eab0336a533259e4348
mv0623 <elfeg@LAPTOP-MV952G>
2024-02-14 10:13:33 +0100

    Version 0 del proyecto

---
+++ src/pintaEmojis/ejercicioProgramacion.java
@@ -0,0 +1,14 @@
+package pintaEmojis;
+import java.util.*;
+
+class ejercicioProgramacion {
+		    public static void main(String[] args) {
+		        ArrayList<Integer> arrayList1= new ArrayList<Integer>();
+		        arrayList1.add(12);
+		        arrayList1.add(13);
+		        arrayList1.add(20);
+		        arrayList1.add(32);
+		        System.out.println(arrayList1.size());
+		  }
+	}
+
