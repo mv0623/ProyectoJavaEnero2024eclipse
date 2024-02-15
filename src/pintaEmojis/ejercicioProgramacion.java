39496cfea28c84f27b36b911c53ecf2853bfe92f
mv0623 <156902469+mv0623@users.noreply.github.com>
2024-02-15 09:52:21 +0100

    Update ejercicioProgramacion.java

--- src/pintaEmojis/ejercicioProgramacion.java
+++ src/pintaEmojis/ejercicioProgramacion.java
@@ -8,6 +8,9 @@ class ejercicioProgramacion {
 		        arrayList1.add(13);
 		        arrayList1.add(20);
 		        arrayList1.add(32);
+			arrayList1.add(24);
+		        arrayList1.add(39);
+			
 		        System.out.println(arrayList1.size());
 		  }
 	}
